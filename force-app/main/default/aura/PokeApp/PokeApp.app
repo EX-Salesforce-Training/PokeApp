a55a3f9cf1b5397c28ce6313f3f4c35cabd3d9ae
ZackFra <zack11frazier22@gmail.com>
2021-08-11 10:01:41 -0400

    poke app

---
+++ force-app/main/default/aura/PokeApp/PokeApp.app
@@ -0,0 +1,3 @@
+<aura:application >
+    <c:Pokedex />
+</aura:application>
---
+++ force-app/main/default/aura/Pokedex/PokedexHelper.js
@@ -0,0 +1,5 @@
+({
+	getPokemonNames : function(i) {
+                                                             
+    }
+})
